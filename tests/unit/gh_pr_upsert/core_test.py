aec5c5fccce873df8bcfa1c19426a881408914e1
Sean Hammond <seanh@users.noreply.github.com>
2022-12-15 19:30:41 +0000

    wip

--- tests/unit/gh_pr_upsert/core_test.py
+++ tests/unit/gh_pr_upsert/core_test.py
@@ -3,6 +3,7 @@ from unittest.mock import call, sentinel
 import pytest
 
 from gh_pr_upsert import core
+from gh_pr_upsert.exceptions import NoChangesError, OtherPeopleError, SameBranchError
 
 
 class TestPRUpsert:
@@ -75,29 +76,117 @@ class TestPRUpsert:
             git.PullRequest.get.return_value.html_url
         )
 
-    def test_it_raises_if_the_base_and_head_branch_are_the_same(self):
-        pass
+    def test_it_raises_if_the_base_and_head_branch_are_the_same(self, base_repo, git):
+        git.GitHubRepo.get.side_effect = [base_repo, base_repo]
+        git.current_branch.return_value = base_repo.default_branch
 
-    def test_if_there_are_no_changes_it_closes_any_existing_pr(self):
-        pass
+        with pytest.raises(SameBranchError):
+            core.pr_upsert(
+                sentinel.remote,
+                sentinel.remote,
+                sentinel.title,
+                sentinel.body,
+                sentinel.close_comment,
+            )
 
-    def test_it_doesnt_close_prs_that_have_other_contributors(self):
-        pass
+    def test_if_there_are_no_changes_it_closes_any_existing_pr(self, git):
+        git.diff.return_value = ""
 
-    def test_if_the_remote_branch_doesnt_exist_it_creates_it(self):
-        pass
+        with pytest.raises(NoChangesError):
+            core.pr_upsert(
+                sentinel.base_remote,
+                sentinel.head_remote,
+                sentinel.title,
+                sentinel.body,
+                sentinel.close_comment,
+            )
 
-    def test_if_the_remote_branch_already_exists_it_updates_it(self):
-        pass
+        git.PullRequest.get.return_value.close.assert_called_once_with(
+            sentinel.close_comment
+        )
 
-    def test_it_doesnt_push_the_remote_branch_if_there_are_other_contributors(self):
-        pass
+    def test_it_doesnt_close_prs_that_have_other_contributors(
+        self, commit_factory, git
+    ):
+        git.diff.return_value = ""
+        git.log.return_value.append(commit_factory())
 
-    def test_it_doesnt_push_the_remote_branch_if_the_changes_are_the_same(self):
-        pass
+        with pytest.raises(NoChangesError):
+            core.pr_upsert(
+                sentinel.base_remote,
+                sentinel.head_remote,
+                sentinel.title,
+                sentinel.body,
+                sentinel.close_comment,
+            )
 
-    def test_if_the_pr_doesnt_exist_it_creates_one(self):
-        pass
+        git.PullRequest.get.return_value.close.assert_not_called()
+
+    def test_if_the_remote_branch_doesnt_exist_it_creates_it(self, git):
+        git.branch_exists.return_value = False
+
+        core.pr_upsert(
+            sentinel.base_remote,
+            sentinel.head_remote,
+            sentinel.title,
+            sentinel.body,
+            sentinel.close_comment,
+        )
+
+        git.push.assert_called_once_with(
+            sentinel.head_remote, git.current_branch.return_value
+        )
+
+    def test_if_the_remote_branch_already_exists_it_updates_it(self, git):
+        git.diff.side_effect = [sentinel.local_diff, sentinel.remote_diff]
+
+        core.pr_upsert(
+            sentinel.base_remote,
+            sentinel.head_remote,
+            sentinel.title,
+            sentinel.body,
+            sentinel.close_comment,
+        )
+
+        git.push.assert_called_once_with(
+            sentinel.head_remote, git.current_branch.return_value
+        )
+
+    def test_it_doesnt_push_the_remote_branch_if_there_are_other_contributors(
+        self, commit_factory, git
+    ):
+        git.diff.side_effect = [sentinel.local_diff, sentinel.remote_diff]
+        git.log.return_value.append(commit_factory())
+
+        with pytest.raises(OtherPeopleError):
+            core.pr_upsert(
+                sentinel.base_remote,
+                sentinel.head_remote,
+                sentinel.title,
+                sentinel.body,
+                sentinel.close_comment,
+            )
+
+        git.push.assert_not_called()
+
+    def test_if_the_pr_doesnt_exist_it_creates_one(self, base_repo, git, head_repo):
+        git.PullRequest.get.return_value = None
+
+        core.pr_upsert(
+            sentinel.base_remote,
+            sentinel.head_remote,
+            sentinel.title,
+            sentinel.body,
+            sentinel.close_comment,
+        )
+
+        git.PullRequest.create.assert_called_once_with(
+            base_repo,
+            head_repo,
+            git.current_branch.return_value,
+            sentinel.title,
+            sentinel.body,
+        )
 
     @pytest.fixture(autouse=True)
     def git(self, mocker, base_repo, head_repo, user, commit_factory):
